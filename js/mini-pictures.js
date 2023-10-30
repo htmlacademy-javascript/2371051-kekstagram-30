432219450fcfebf7c2b66660f4eceeda396b4920
Tatyana <tania-300@mail.ru>
2023-10-30 22:34:24 +0700

    Создает дом-элементы и заполняет их

--- js/mini-pictures.js
+++ js/mini-pictures.js
@@ -9,15 +9,26 @@
 // Подключите модуль в проект.
 import { createPhotosArray } from './data.js';
 
-const photoArray = createPhotosArray();
+const photosArray = createPhotosArray();
 const picturesTemplate = document.querySelector('#picture').content;
 const pictureLink = picturesTemplate.querySelector('.picture');
 const picturesContainer = document.querySelector('.pictures');
 
-photoArray.forEach((photo) => {
+const picturesFragment = document.createDocumentFragment();
+
+photosArray.forEach((photo) => {
   const newPhoto = pictureLink.cloneNode(true);
   const newPhotoImage = newPhoto.querySelector('.picture__img');
+  const newPhotoLikes = newPhoto.querySelector('.picture__likes');
+  const newPhotoComments = newPhoto.querySelector('.picture__comments');
+
   newPhotoImage.src = photo.url;
-  picturesContainer.append(newPhoto);
+  newPhotoImage.alt = photo.description;
+  newPhotoLikes.textContent = photo.likes;
+  newPhotoComments.textContent = photo.comments.length;
+
+  picturesFragment.append(newPhoto);
 });
+
+picturesContainer.append(picturesFragment);
 
